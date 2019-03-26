4324374042c9bcd5440da281a3cde0d5fff4ae4c
Rory Gaddin <rory.gaddin@stanchionpayments.com>
2019-03-26 05:02:31 +0200

    Fixed actions list

--- src/store/actions/habit.actions.js
+++ src/store/actions/habit.actions.js
@@ -1,4 +1,4 @@
-export const REQUEST_NEW_HABIT_FORM = 'add-habit';
-export const REFRESH_HABIT_LIST = 'add-habit';
+export const REQUEST_NEW_HABIT_FORM = 'request-new-habit-form';
+export const REFRESH_HABIT_LIST = 'refresh-list';
 export const SAVE_HABIT = 'save-habit';
 export const CHANGE_DISPLAY_STATE = 'change-display-state';
